f73e07c6deaeceaa79a84e7e65765b89fdd1b2a4
root <root@DESKTOP-NADUE7G.localdomain>
2018-05-08 00:26:27 -0700

    added an updater

--- Updater.py
+++ Updater.py
@@ -1,8 +1,33 @@
 from subprocess import call
-import Version as V1
-call(['mv', 'PiCar/Version.py', 'Tmp.py'])
-import Tmp as V2
-if V2.version > V1.Version:
-	# Need to update
-	pass
-call(['rm', 'Tmp.py'])
+import Variables as V1
+import os
+
+def update():        
+        call(['git', 'clone', '-q', 'https://www.github.com/ImMilesAhead/PiCar'])
+        call(['cp', 'PiCar/Variables.py', 'Tmp.py'])
+        import Tmp as V2
+        # Compare newly downloaded version to current version
+        if V2.version > V1.version:
+                # Need to update
+                print('Need to update')
+                files = os.listdir('./PiCar')
+                shFile = open('update.sh', 'w')
+                for f in files:
+                        if not f[0] == '.':
+                                # Add line to new sh file
+                                shFile.write('cp PiCar/' + f + ' ./\n')
+                shFile.write('rm Tmp.py\n')
+                shFile.write('rm -r PiCar\n')
+                shFile.write('echo Done!\n')
+                shFile.close()
+                print('chmodding')
+                call(['chmod', '+x', 'update.sh'])
+                call(['sh', 'update.sh'])
+                call(['rm', 'update.sh'])
+        else:
+                print('Version is up to date at version: ' + str(V2.version))
+                call(['rm', 'Tmp.py'])
+                call(['rm', '-r', 'PiCar'])
+
+if __name__ == '__main__':
+        update()
